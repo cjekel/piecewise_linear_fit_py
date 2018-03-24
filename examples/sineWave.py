f0a054bdd73ae567577ab0d360dec5ce808cc9a8
Vladimir Khodygo <khodygo@gmail.com>
2018-03-24 14:12:55 +0000

    pep8 examples, fixed typos

--- examples/sineWave.py
+++ examples/sineWave.py
@@ -1,35 +1,35 @@
-#   fit for a specified number of line segments
-#   you specify the number of line segments you want, the library does the rest
+# fit for a specified number of line segments
+# you specify the number of line segments you want, the library does the rest
 
-#    import our libraires
+# import our libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pwlf
 
-#   generate sin wave data
+# generate sin wave data
 x = np.linspace(0, 10, num=100)
-y = np.sin(x*np.pi/2)
-#   add noise to the data
-y = np.random.normal(0,0.05, 100) + y
+y = np.sin(x * np.pi / 2)
+# add noise to the data
+y = np.random.normal(0, 0.05, 100) + y
 
-#   initialize piecwise linear fit with your x and y data
-myPWLF = pwlf.piecewise_lin_fit(x,y)
+# initialize piecewise linear fit with your x and y data
+myPWLF = pwlf.piecewise_lin_fit(x, y)
 
-#   fit the data for four line segments
-res = myPWLF.fit(16,disp=True)
-#   i'm passing the argument disp=True to see the progress of the differential
-#   evoultion so you can be sure the program isn't just hanging...
+# fit the data for four line segments
+res = myPWLF.fit(16, disp=True)
+# i'm passing the argument disp=True to see the progress of the differential
+# evolution so you can be sure the program isn't just hanging...
 
-#   Be patient! this one takes some time - It's a difficult problem
-#   using this differential evolution algo + bfgs can be over 500,000.0 function
-#   evaluations
+# Be patient! this one takes some time - It's a difficult problem
+# using this differential evolution algo + bfgs can be over 500,000.0 function
+# evaluations
 
-#   predict for the determined points
+# predict for the determined points
 xHat = np.linspace(min(x), max(x), num=10000)
 yHat = myPWLF.predict(xHat)
 
-#   plot the results
+# plot the results
 plt.figure()
-plt.plot(x,y,'o')
-plt.plot(xHat,yHat, '-')
+plt.plot(x, y, 'o')
+plt.plot(xHat, yHat, '-')
 plt.show()
