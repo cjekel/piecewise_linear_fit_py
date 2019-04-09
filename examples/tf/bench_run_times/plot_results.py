472a9bcd899699ad1e1f425b448d08d4eac95a84
Charles Jekel <cjekel@gmail.com>
2019-04-08 22:28:57 -0400

    add plots from all machines

--- examples/tf/bench_run_times/plot_results.py
+++ examples/tf/bench_run_times/plot_results.py
@@ -7,53 +7,64 @@ import matplotlib.pyplot as plt
 # probability for engineers. Sixth edition. John Wiley & Sons.
 k = 2.535
 
-amd_fx_run_times = np.load('amd_fx_8350_titanXP/6_break_times.npy')
-n = np.load('amd_fx_8350_titanXP/n.npy')
-amd_fx_run_times_means = amd_fx_run_times.mean(axis=2)
-amd_fx_run_times_stds = amd_fx_run_times.std(axis=2, ddof=1)
+
+def load_and_plot_data(run_strs, n_strs, j=1):
+
+    run_times = np.load(run_strs)
+    n = np.load(n_strs)
 
 
 
-plt.figure()
-plt.grid()
-plt.errorbar(n, amd_fx_run_times_means[0], yerr=k*amd_fx_run_times_stds[0], capsize=2.0, label='Numpy')
-plt.errorbar(n, amd_fx_run_times_means[1], yerr=k*amd_fx_run_times_stds[1], capsize=2.0, label='TF GPU')
-plt.errorbar(n, amd_fx_run_times_means[2], yerr=k*amd_fx_run_times_stds[2], capsize=2.0, label='TF CPU')
-
-plt.xlabel('Number of data points')
-plt.ylabel('Run time (seconds, Lower is better)')
-plt.semilogx()
-
-plt.figure()
-plt.grid()
-
-plt.errorbar(n[1:], amd_fx_run_times_means[0, 1:] - amd_fx_run_times_means[0, 1:], yerr=(k*amd_fx_run_times_stds[0, 1:]), capsize=2.0, label='Numpy')
-plt.errorbar(n[1:], amd_fx_run_times_means[1, 1:] - amd_fx_run_times_means[0, 1:], yerr=(k*amd_fx_run_times_stds[1, 1:]), capsize=2.0, label='TF GPU')
-plt.errorbar(n[1:], amd_fx_run_times_means[2, 1:] - amd_fx_run_times_means[0, 1:], yerr=(k*amd_fx_run_times_stds[2, 1:]), capsize=2.0, label='TF CPU')
-
-plt.xlabel('Number of data points')
-plt.ylabel('Run time difference (Lower is better)')
-plt.semilogx()
+    run_times_means = run_times.mean(axis=2)
+    run_times_stds = run_times.std(axis=2, ddof=1)
 
 
-plt.figure()
-plt.grid()
 
-plt.errorbar(n[1:], amd_fx_run_times_means[0, 1:]/amd_fx_run_times_means[0, 1:], yerr=(k*amd_fx_run_times_stds[0, 1:])/amd_fx_run_times_means[0, 1:], capsize=2.0, label='Numpy')
-plt.errorbar(n[1:], amd_fx_run_times_means[1, 1:]/amd_fx_run_times_means[0, 1:], yerr=(k*amd_fx_run_times_stds[1, 1:])/amd_fx_run_times_means[0, 1:], capsize=2.0, label='TF GPU')
-plt.errorbar(n[1:], amd_fx_run_times_means[2, 1:]/amd_fx_run_times_means[0, 1:], yerr=(k*amd_fx_run_times_stds[2, 1:])/amd_fx_run_times_means[0, 1:], capsize=2.0, label='TF CPU')
-plt.xlabel('Number of data points')
-plt.ylabel('Run time relative to Numpy (Lower is better)')
-plt.semilogx()
+    plt.figure()
+    plt.grid()
+    plt.errorbar(n, run_times_means[0], yerr=k*run_times_stds[0], capsize=2.0, label='Numpy')
+    # plt.errorbar(n, run_times_means[1], yerr=k*run_times_stds[1], capsize=2.0, label='TF GPU')
+    plt.errorbar(n, run_times_means[j], yerr=k*run_times_stds[j], capsize=2.0, label='TF CPU')
+
+    plt.xlabel('Number of data points')
+    plt.ylabel('Run time (seconds, Lower is better)')
+    plt.semilogx()
+
+    plt.figure()
+    plt.grid()
+
+    plt.errorbar(n[1:], run_times_means[0, 1:] - run_times_means[0, 1:], yerr=(k*run_times_stds[0, 1:]), capsize=2.0, label='Numpy')
+    # plt.errorbar(n[1:], run_times_means[1, 1:] - run_times_means[0, 1:], yerr=(k*run_times_stds[1, 1:]), capsize=2.0, label='TF GPU')
+    plt.errorbar(n[1:], run_times_means[j, 1:] - run_times_means[0, 1:], yerr=(k*run_times_stds[j, 1:]), capsize=2.0, label='TF CPU')
+
+    plt.xlabel('Number of data points')
+    plt.ylabel('Run time difference (Lower is better)')
+    plt.semilogx()
 
 
-plt.figure()
-plt.grid()
-plt.errorbar(n[1:], amd_fx_run_times_means[0, 1:]/amd_fx_run_times_means[1, 1:], yerr=(k*amd_fx_run_times_stds[0, 1:])/amd_fx_run_times_means[1, 1:], capsize=2.0, label='Numpy')
-plt.errorbar(n[1:], amd_fx_run_times_means[1, 1:]/amd_fx_run_times_means[1, 1:], yerr=(k*amd_fx_run_times_stds[1, 1:])/amd_fx_run_times_means[1, 1:], capsize=2.0, label='TF GPU')
-plt.errorbar(n[1:], amd_fx_run_times_means[2, 1:]/amd_fx_run_times_means[1, 1:], yerr=(k*amd_fx_run_times_stds[2, 1:])/amd_fx_run_times_means[1, 1:], capsize=2.0, label='TF CPU')
-plt.xlabel('Number of data points')
-plt.ylabel('Run time relative to TF GPU (Lower is better)')
-plt.semilogx()
+    plt.figure()
+    plt.grid()
 
-plt.show()
+    plt.errorbar(n[1:], run_times_means[0, 1:]/run_times_means[0, 1:], yerr=(k*run_times_stds[0, 1:])/run_times_means[0, 1:], capsize=2.0, label='Numpy')
+    # plt.errorbar(n[1:], run_times_means[1, 1:]/run_times_means[0, 1:], yerr=(k*run_times_stds[1, 1:])/run_times_means[0, 1:], capsize=2.0, label='TF GPU')
+    plt.errorbar(n[1:], run_times_means[j, 1:]/run_times_means[0, 1:], yerr=(k*run_times_stds[j, 1:])/run_times_means[0, 1:], capsize=2.0, label='TF CPU')
+    plt.xlabel('Number of data points')
+    plt.ylabel('Run time relative to Numpy (Lower is better)')
+    plt.semilogx()
+
+
+    plt.figure()
+    plt.grid()
+    plt.errorbar(n[1:], run_times_means[0, 1:]/run_times_means[1, 1:], yerr=(k*run_times_stds[0, 1:])/run_times_means[1, 1:], capsize=2.0, label='Numpy')
+    # plt.errorbar(n[1:], run_times_means[1, 1:]/run_times_means[1, 1:], yerr=(k*run_times_stds[1, 1:])/run_times_means[1, 1:], capsize=2.0, label='TF GPU')
+    plt.errorbar(n[1:], run_times_means[j, 1:]/run_times_means[1, 1:], yerr=(k*run_times_stds[j, 1:])/run_times_means[1, 1:], capsize=2.0, label='TF CPU')
+    plt.xlabel('Number of data points')
+    plt.ylabel('Run time relative to TF GPU (Lower is better)')
+    plt.semilogx()
+
+    plt.show()
+
+
+load_and_plot_data('amd_fx_8350_titanXP/6_break_times.npy', 'amd_fx_8350_titanXP/n.npy', j=2)
+load_and_plot_data('amd_ryzen_2700x/6_break_times.npy', 'amd_ryzen_2700x/n.npy')
+load_and_plot_data('intel_i5_6300u/6_break_times.npy', 'intel_i5_6300u/n.npy')
