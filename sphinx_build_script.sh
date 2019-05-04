f14369280d095d50e462950df9e3740cc1a0d1bf
cjekel <cjekel@gmail.com>
2019-05-04 14:03:37 -0400

    don't sed yourselfgit add .!

--- sphinx_build_script.sh
+++ sphinx_build_script.sh
@@ -11,4 +11,6 @@ make html
 # copy sphinx build into docs and doctrees
 cp -r build/doctrees/* ../doctrees/
 cp -r build/html/* ../docs/
-grep -rl -e "break-word" | xargs sed -i 's:break-word:break-all:g'
+# grep -rl -e "word-break: break-word" --exclude=sphinx_build_script.sh | xargs sed -i 's/word-break: break-word/word-break: break-all/g'
+cd ..
+sed -i 's/word-break: break-word/word-break: break-all/g' docs/_static/basic.css 
