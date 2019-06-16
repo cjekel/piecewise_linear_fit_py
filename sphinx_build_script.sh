a7ea1ec1c4a8fd0c55ab12739c2abda6f3ede454
cjekel <cjekel@gmail.com>
2019-06-15 20:06:18 -0400

    tweak sphinx build script

--- sphinx_build_script.sh
+++ sphinx_build_script.sh
@@ -12,5 +12,5 @@ make html
 cp -r build/doctrees/* ../doctrees/
 cp -r build/html/* ../docs/
 # grep -rl -e "word-break: break-word" --exclude=sphinx_build_script.sh | xargs sed -i 's/word-break: break-word/word-break: break-all/g'
-cd ..
-sed -i 's/break-word/break-all/g' docs/_static/basic.css 
+#cd ..
+#sed -i 's/break-word/break-all/g' docs/_static/basic.css 
