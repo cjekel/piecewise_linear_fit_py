676360168cf0d627d8f6861e920ab408feaf9cfa
cjekel <cjekel@gmail.com>
2020-02-06 13:33:00 -0800

    roll pwlf version to 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pwlf',
-    version='1.1.7',
+    version='1.2.0',
     author='Charles Jekel',
     author_email='cjekel@gmail.com',
     packages=['pwlf'],
