417f13ae2036bb078bfbe9b8d43411c605be6121
cjekel <cjekel@gmail.com>
2020-03-31 17:07:24 -0700

    sometimes copy pasta breaks things

--- pwlf/pwlf.py
+++ pwlf/pwlf.py
@@ -1427,32 +1427,11 @@ class PiecewiseLinFit(object):
             raise AttributeError(errmsg)
         k = self.beta.size - 1
 
-        if method == 'linear':# perform an ordinary pwlf fit to the entire data
-my_pwlf = pwlf.PiecewiseLinFit(X.flatten(), y.flatten())
-my_pwlf.fit(n_segments)
+        if method == 'linear':
+             self.standard_errors()
+             # calculate my t-value
+             t = self.beta / self.se
 
-# compute the standard deviation in y
-y_std = np.std(y, axis=0)
-# set the weights to be one over the standard deviation
-weights = 1.0 / y_std
-
-# perform a weighted least squares to the data
-my_pwlf_w = pwlf.PiecewiseLinFit(x, y.mean(axis=0), weights=weights)
-my_pwlf_w.fit(n_segments)
-
-
-# compare the fits
-xhat = np.linspace(0, 10, 1000)
-yhat = my_pwlf.predict(xhat)
-yhat_w = my_pwlf_w.predict(xhat)
-t1 = time()
-print('Runtime:', t1-t0)
-plt.figure()
-plt.plot(X.flatten(), y.flatten(), '.')
-plt.plot(xhat, yhat, '-', label='Ordinary LS')
-plt.plot(xhat, yhat_w, '-', label='Weighted LS')
-plt.legend()
-plt.show()f.se
         elif method == 'non-linear':
             nb = self.beta.size + self.fit_breaks.size - 2
             k = nb - 1
@@ -1586,29 +1565,3 @@ plt.show()f.se
             L = np.inf
             # something went wrong...
         return L
-# perform an ordinary pwlf fit to the entire data
-my_pwlf = pwlf.PiecewiseLinFit(X.flatten(), y.flatten())
-my_pwlf.fit(n_segments)
-
-# compute the standard deviation in y
-y_std = np.std(y, axis=0)
-# set the weights to be one over the standard deviation
-weights = 1.0 / y_std
-
-# perform a weighted least squares to the data
-my_pwlf_w = pwlf.PiecewiseLinFit(x, y.mean(axis=0), weights=weights)
-my_pwlf_w.fit(n_segments)
-
-
-# compare the fits
-xhat = np.linspace(0, 10, 1000)
-yhat = my_pwlf.predict(xhat)
-yhat_w = my_pwlf_w.predict(xhat)
-t1 = time()
-print('Runtime:', t1-t0)
-plt.figure()
-plt.plot(X.flatten(), y.flatten(), '.')
-plt.plot(xhat, yhat, '-', label='Ordinary LS')
-plt.plot(xhat, yhat_w, '-', label='Weighted LS')
-plt.legend()
-plt.show()
