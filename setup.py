d85c4ea98e0cdcfcdfa0041587f2717ca2cc5c6d
cjekel <cjekel@gmail.com>
2020-04-02 15:31:24 -0700

    stage for 2.0.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pwlf',
-    version='1.2.0',
+    version='2.0.0',
     author='Charles Jekel',
     author_email='cjekel@gmail.com',
     packages=['pwlf'],
