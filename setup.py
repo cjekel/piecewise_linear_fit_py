6f8edb100d74db8f589f0f452e8f637561244d99
cjekel <cjekel@gmail.com>
2020-05-08 15:19:15 -0700

    roll version for this experiment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pwlf',
-    version='2.0.0',
+    version='2.0.1',
     author='Charles Jekel',
     author_email='cjekel@gmail.com',
     packages=['pwlf'],
