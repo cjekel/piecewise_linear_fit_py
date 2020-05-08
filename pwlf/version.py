72ab99cc0bdfcaf85b8b7f05b15fb29120430702
cjekel <cjekel@gmail.com>
2020-05-08 15:18:19 -0700

    remove try;exception arround metadata.version

--- pwlf/version.py
+++ pwlf/version.py
@@ -4,7 +4,4 @@ except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
 
-try:
-    __version__ = metadata.version('pwlf')
-except Exception:
-    __version__ = 'unknown'
+__version__ = metadata.version('pwlf')
