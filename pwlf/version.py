0b0eb46c3ac22e656cbca77f51403b0888ea8584
cjekel <cjekel@gmail.com>
2020-05-25 09:28:38 -0700

    version.py versioning...

--- pwlf/version.py
+++ pwlf/version.py
@@ -1,7 +1,1 @@
-try:
-    from importlib import metadata
-except ImportError:
-    # Running on pre-3.8 Python; use importlib-metadata package
-    import importlib_metadata as metadata
-
-__version__ = metadata.version('pwlf')
+__version__ = "2.0.2"
